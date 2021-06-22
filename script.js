464b3056d0e9c1de107663ef4e48498dd662fdc1
Debora Teodorico <debora.steodorico@gmail.com>
2021-06-22 14:24:05 -0300

    Adicionado mudancas de js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ button.addEventListener('click', () => {
   const text = inputLogin.value;
   const inputPassword = document.querySelector('#input-password');
   const password = inputPassword.value;
-  if (text === textLogin || password === textPassword) {
+  if (text === textLogin && password === textPassword) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
